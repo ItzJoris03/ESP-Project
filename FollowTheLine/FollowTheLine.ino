828d3c458edc0eb2dc4edc2aabd9a490e9a6e63e
ItzJoris03 <jorishummel03@gmail.com>
2022-06-01 14:58:06 +0200

    Identified Motor Pins

--- FollowTheLine/FollowTheLine.ino
+++ FollowTheLine/FollowTheLine.ino
@@ -1,6 +1,16 @@
+#include <analogWrite.h>
+
 void setup() {
   // put your setup code here, to run once:
+  int motorRB = 5; // Right Wheel Backwards
+  int motorLF = 16; // Left Wheen Forwards
+  int motorLB = 17; // Left Wheel Backwards
+  int motorRF = 18; // Right Wheel Forwards
 
+  analogWrite(motorRB, 0);
+  analogWrite(motorLF, 0);
+  analogWrite(motorLB, 0);
+  analogWrite(motorRF, 0);
 }
 
 void loop() {
